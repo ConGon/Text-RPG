bf51c611994c2d5228d5cbe6514cfddd0ec1c65f
ConGon <connorgoodwin999@gmail.com>
2024-09-01 12:40:01 -0300

    commit epic

---
+++ Text-Based-RPG.py
@@ -0,0 +1,73 @@
+import math
+import random
+
+# my_dict = {
+#     'name': 'Alice',
+#     'age': 30,
+#     'city': 'New York'
+# }
+# print (my_dict['name'])
+
+levelData = [
+    100,
+    200,
+    300,
+    400,
+    500
+]
+
+playerData = {
+    "Health": 100,
+    "Attack": 10,
+    "Defence": 5,
+    "Level": 0,
+    "Exp": 0,
+    "Gold": 0
+}
+
+enemies = {
+    "Goblin": {
+        "Health": 25,
+        "Attack": 5,
+        "ExpValue": 10
+    },
+    "Skeleton": {
+        "Health": 50,
+        "Attack": 10,
+        "ExpValue": 35
+    }
+
+}
+
+randomEncounters = {
+    "TreasureChest": {
+        "Exp": 0,
+        "Gold:": 25
+    },
+    "AncientStatue": {
+        "Exp": 100,
+        "Attack": 5
+    }
+}
+# random_item = random.choice(list(plrData.keys()))
+
+# randomChoice = plrData[random_item]
+
+plrName = str(input("What is your name"))
+
+def gameLoop():
+    
+
+    
+
+
+
+
+
+    
+
+
+
+
+    
+
